2ce72054e695c228aba03afccdc06400d1d76931
Manish <86160294+codereyinish@users.noreply.github.com>
2024-02-05 20:57:37 -0500

    <some git message here>

--- src/main/java/com/example/marginalutility/MarginalUtility.java
+++ src/main/java/com/example/marginalutility/MarginalUtility.java
@@ -60,17 +60,23 @@ public class MarginalUtility {
         System.out.println("Enter the unit price of Movies: $ ");
         double Munit =  scanf.nextDouble();
 //        Since I didnt see Math.floorDiv(with double arguments)
-        double div = balance/Tunit;
-        int maxT = (int) Math.floor(div);
+        double div1 = balance/Tunit;
+        int maxT = (int) Math.floor(div1);
 //        maxCTU = maximum CTU, where CTU = combined TU, where TU is total utilties of both respective quantities
         double maxCTU = 0;
         //            Let o_t and o_m be the optimal combination
         int o_t = 0;
         int o_m = 0; //because  0 means no purchase at all initially for balance = $0
+//              to find size of combination array, we need size. For this .  we have maxT, lets find maxN
+        double  div2 = balance/Munit;
+        int maxM = (int) Math.floor(div2);
+
         for(int n_t = 0; n_t<= maxT; n_t++ ) //variable name can't be special character like (), underscore _ allowed
         {
             double  Texpense = n_t * Tunit;
             int n_m = (int) Math.floor((balance - Texpense)/Munit);
+//            Storing combination after calculating n_m;
+
             double CTU = Tees[n_t] + Movies[n_m];
             if(CTU>maxCTU)
             {
@@ -78,19 +84,42 @@ public class MarginalUtility {
                 o_t = n_t;
                 o_m = n_m;
             }
-            //sometimes we might have two optimal combinations
-            else if(CTU == maxCTU )
-            {
-                System.out.println("The best combination of spending for maximum combined total utility  is " + n_t + " tees and " + n_m + " movies  ");
-            }
-//            which is the best combination then
 
         }
         System.out.println("The maximum  total utility that can be gained by purchased by buying set of Tees and movies is:  " + maxCTU);
-        System.out.println("The best combination of spending for maximum combined total utility  is " + o_t + " tees and " + o_m + " movies " );
+//    best combination of Tees and Movies
 
 
     }
+    public class Combination {
+        private int T;
+        private int M;
+
+        public Combination(int T, int M) {
+            this.T = T;
+            this.M = M;
+        }
+
+        //I am not gonna change the 'T' and 'M' in the near future, so no need of Set(.....);
+        public int getT() {
+            return T;
+        }
+
+        public int getM() {
+            return M;
+        }
+
+        /*Deep cloning of complex object is resource intensive(time and memory). THough it is not complex
+        what we are doing, we still dont use it for minimalism.. See  objects are not going to be modified later
+        intentionally , and top of that we have set instance variables(T and M) as private so we need set(..args) methods
+         */
+    }
+
+
+
+
+    }
+
 
 
 
